b1beafd5243aed031315586681424dc01bc86ed6
Phil Piwonka <wnka@users.noreply.github.com>
2023-12-02 23:38:44 -0800

    parsing day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,6 +2,17 @@ use std::{fs::File, io::{BufRead, BufReader}};
 
 use clap::Parser;
 
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    bytes::complete::take_while1,
+    character::complete::{newline, space0, u64, alphanumeric0},
+    combinator::{map, all_consuming},
+    multi::separated_list0,
+    sequence::{delimited, pair, preceded, terminated, tuple, separated_pair},
+    IResult,
+};
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -17,6 +28,25 @@ struct Args {
 enum ParseError {
     Error
 }
+
+#[derive(Debug)]
+struct Game {
+    id: u64,
+    grabs: Vec<Grab>
+}
+
+#[derive(Debug)]
+struct GrabEntry {
+    count: u64,
+    color: String,
+}
+
+#[derive(Debug)]
+struct Grab {
+    grabs: Vec<GrabEntry>
+}
+
+
 
 fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead {
     let mut ranges = Vec::new();
@@ -37,8 +67,37 @@ fn main() -> Result<(), ParseError> {
     let input_ranges = parse(BufReader::new(input_file))?;
 
     for range in input_ranges {
-        println!("{}", range);
+        let parsed = all_consuming(line_parser)(&range);
+        println!("{:?}", parsed);
     }
 
     Ok(())
+}
+
+fn line_parser(s: &str) -> IResult<&str, Game> {
+    map(
+        tuple((
+        delimited(tag("Game "), u64, tag(": ")),
+        separated_list0(tag("; "), parse_grab)
+        ))
+    ,
+    |(id, grabs)| {
+        println!("grabs {:?}", grabs);
+        Game { id, grabs }
+    }
+    )(s)
+}
+
+fn parse_grab(i: &str) -> IResult<&str, Grab> {
+    map(
+        separated_list0(tag(", "), parse_grab_entry),
+        |grabs| Grab { grabs }
+    )(i)
+} 
+
+fn parse_grab_entry(i: &str) -> IResult<&str, GrabEntry> {
+    map(
+        separated_pair(u64, tag(" "), alphanumeric0),
+        |(count, color)| { GrabEntry {count: count, color: String::from(color)} }
+    )(i)
 }
