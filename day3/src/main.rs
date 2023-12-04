9909956046e608432c3f02727cafabae701068a0
Phil Piwonka <wnka@users.noreply.github.com>
2023-12-03 22:05:10 -0800

    Start on day 3

---
+++ day3/src/main.rs
@@ -0,0 +1,57 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+use clap::Parser;
+
+use regex::Regex;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+/// Command line args
+struct Args {
+    /// Filename to read
+    #[arg(short, long)]
+    input: String,
+
+    /// Which part of the day we're solving
+    /// Usually only 1 or 2
+    /// Defaults to 1
+    #[arg(short, long, default_value_t = 1)]
+    part: u8,
+}
+
+#[derive(Debug)]
+enum ParseError {
+    Error
+}
+
+/// Read an input file and return a Ok(Vec<String>) with one String per line
+/// If something weird happens, return Err(ParseError::Error)
+fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead {
+    let mut result = Vec::new();
+    let lines = BufReader::new(input_buffer).lines();
+    for line in lines {
+        match line {
+            Ok(s) => result.push(s),
+            Err(_) => return Err(ParseError::Error)
+        }
+    }
+    Ok(result)
+}
+
+fn main() -> Result<(), ParseError> {
+    let args = Args::parse();
+
+    let input_file = File::open(args.input).unwrap();
+    let input_ranges = parse(BufReader::new(input_file))?;
+
+    for range in input_ranges {
+        let numbers_re = Regex::new("[0-9]+").unwrap();
+        for extract in numbers_re.captures_iter(&range).map(|e| e.get(0)) {
+            println!("{:?}", extract.unwrap());
+        }
+
+        println!("{}", range);
+    }
+
+    Ok(())
+}
