131d82a423178570575d8e04e83d1cdca601fd84
Phil Piwonka <wnka@users.noreply.github.com>
2023-12-06 18:50:21 -0800

    day 5: parsing done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,14 +1,14 @@
-use std::{fs::File, io::{BufRead, BufReader, Read}, str, ops::Range};
+use std::{fs::File, io::{BufRead, BufReader, Read}, str, ops::Range, collections::VecDeque};
 
 use clap::Parser;
 
 use nom::{
-    bytes::complete::{tag, is_not, take_until},
-    character::complete::{u64, multispace0, multispace1, alphanumeric1, space1, one_of},
-    combinator::{map, all_consuming, map_res},
-    multi::{separated_list0, separated_list1, many1},
-    sequence::{delimited, tuple, preceded, terminated, pair},
-    IResult, branch::alt,
+    bytes::complete::tag,
+    character::complete::{u64, alphanumeric1},
+    combinator::{map, all_consuming},
+    multi::separated_list1,
+    sequence::tuple,
+    IResult,
 };
 
 #[derive(Parser, Debug)]
@@ -35,9 +35,10 @@ enum ParseError {
 struct Block {
     from: String,
     to: String,
-    lines: Vec<Vec<u64>>
+    ranges: Vec<Ranges>
 }
 
+#[derive(Debug)]
 struct Ranges {
     source: Range<u64>,
     destination: Range<u64>
@@ -50,57 +51,66 @@ fn chunk_parser(s: &str) -> IResult<&str, Block> {
         tag("-to-"),
         alphanumeric1,
         tag(" map:\n"),
-        //separated_list0(tag("\n"), test)
-        //separated_list0(tag("\n"), test)
         separated_list1(tag("\n"),
-        separated_list1(tag(" "),u64))
+        parse_ranges)
         ))
     , |(a, _, b, _, c)| {
-        println!("{:?}", c);
-        Block { from: String::from(a), to: String::from(b), lines: c}
+        Block { from: String::from(a), to: String::from(b), ranges: c}
     })(s)
 }
 
-/*
 fn parse_ranges(s: &str) -> IResult<&str, Ranges> {
-    
+    map(
+    separated_list1(tag(" "), u64),
+    |v| {
+        assert_eq!(v.len(), 3);
+        let s_start = *v.get(1).unwrap();
+        let d_start = *v.get(0).unwrap();
+        let run = v.get(2).unwrap();
+        Ranges { source: s_start..(s_start+run), destination: d_start..(d_start+run) }
+    }
+    )(s)
 }
-*/
 
-fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead {
+fn parse_seeds(s: &str) -> IResult<&str, Vec<u64>> {
+    map(
+        tuple((
+            tag("seeds: "),
+            separated_list1(tag(" "), u64)
+        )),
+        |(_, v)| { v }
+    )(s)
+}
+fn parse<T>(input_buffer: T) -> Result<(Vec<u64>, Vec<Block>), ParseError> where T: BufRead {
     let mut reader = BufReader::new(input_buffer);
     let mut block = vec![];
     reader.read_to_end(&mut block).unwrap();
     let file_contents = str::from_utf8(&block).unwrap();
-    let blocks = file_contents.split("\n\n");
-    for block in blocks {
-        println!("BLOCK: \"{}\" END BLOCK", block);
-        println!("{:?}", chunk_parser(&block));
-    }
-    Ok(Vec::new())
+    let mut blocks : VecDeque<&str> = file_contents.split("\n\n").collect();
+    let seeds: Vec<u64> = match all_consuming(parse_seeds)(blocks.pop_front().unwrap()) {
+        Ok(s) => s.1,
+        Err(e) => panic!("Seed Parser Problem! {:?}", e)
+    };
+    let blocks = blocks.into_iter().map(|b| {
+        match chunk_parser(b) {
+            Ok(s) => s.1,
+            Err(e) => panic!("Parser problem! {:?}", e)
+        }
+    }).collect();
+    Ok((seeds, blocks))
 }
 
 fn main() -> Result<(), ParseError> {
     let args = Args::parse();
 
     let input_file = File::open(args.input).unwrap();
-    let input_ranges = parse(BufReader::new(input_file))?;
+    let (seeds, blocks) = parse(BufReader::new(input_file))?;
 
-    for range in input_ranges {
-        println!("{}", range);
+    println!("seeds: {:?}", seeds);
+
+    for range in blocks {
+        println!("{:?}", range);
     }
-
-    let test = "seed-to-soil map:";
-    /*
-    map(
-        tuple((
-            is_not("-"),
-            tag("to-"),
-            delimited(alphanumeric1, multispace1, tag("map:"))
-        )),
-        |(a,b,c)| { println!("a {}, b {}", a,b)}
-    )(test);
-    */
 
     Ok(())
 }
