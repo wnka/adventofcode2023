2ebee4cb529224b60c606e9c3121007a114f0969
Phil Piwonka <wnka@users.noreply.github.com>
2023-12-08 00:02:19 -0800

    day 8 done I had to get help on this one, the brute-force was crazy

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -18,7 +18,7 @@ struct Args {
     /// Filename to read
     #[arg(short, long)]
     input: String,
-
+    
     /// Which part of the day we're solving
     /// Usually only 1 or 2
     /// Defaults to 1
@@ -48,12 +48,12 @@ fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead
 fn branch_parser(s: &str) -> IResult<&str, (&str, &str, &str)> {
     map(
         tuple((
-        alphanumeric1,
-        tag(" = ("),
-        alphanumeric1,
-        tag(", "),
-        alphanumeric1,
-        tag(")")
+            alphanumeric1,
+            tag(" = ("),
+            alphanumeric1,
+            tag(", "),
+            alphanumeric1,
+            tag(")")
         )),
         |(key, _, left, _, right, _)| {
             (key, left, right)
@@ -61,44 +61,62 @@ fn branch_parser(s: &str) -> IResult<&str, (&str, &str, &str)> {
     )(s)
 }
 
+fn gcd(mut x: u64, mut y: u64) -> u64 {
+    while x != y {
+        if x > y {
+            x = x - y;
+        } else {
+            y = y - x;
+        }
+    }
+    x
+}
+
+fn lcm(x: u64, y: u64) -> u64 {
+    x * y / gcd(x, y)
+}
+
 fn main() -> Result<(), ParseError> {
     let args = Args::parse();
-
+    
     let input_file = File::open(args.input).unwrap();
     let input_ranges = parse(BufReader::new(input_file))?;
-
-    let steps = input_ranges.get(0).unwrap().chars();
-
+    
+    let steps = input_ranges.get(0).unwrap();
+    
     let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut currents = vec![];
+    
     for range in input_ranges[2..].iter() {
         match all_consuming(branch_parser)(range) {
             Ok(p) => {
                 map.insert(p.1.0, (p.1.1, p.1.2));
+                if p.1.0.ends_with('A') {
+                    currents.push(p.1.0);
+                }
             },
             Err(e) => panic!("Parsing error! {}", e)
         }
     }
-
-    let mut current = "AAA";
-    let mut step_count = 0;
-    for i in steps.cycle() {
-        let next = map.get(current).unwrap();
-        match i {
-            'L' => current = next.0,
-            'R' => current = next.1,
-            _ => panic!("Unknown step!")
-        };
-        step_count += 1;
-        if current == "ZZZ" { break }
+    
+    println!("Starting points: {}", currents.len());
+    
+    let mut total_dist = 1;
+    for one in &currents {
+        let mut current = *one;
+        for (step, d) in steps.bytes().cycle().enumerate() {
+            let next = map.get(current).unwrap();
+            match d {
+                b'L' => current = next.0,
+                b'R' => current = next.1,
+                _ => panic!("Unknown step!")
+            };
+            if current.ends_with('Z') {
+                total_dist = lcm(total_dist, u64::try_from(step+1).unwrap());
+                break
+            }
+        }        
     }
-
-    println!("Steps: {}", step_count);
-
-
-//    let test = "LLR";
-//    for i in test.chars().cycle() {
- //       println!("{}", i);
- //   }
-
+    println!("Steps: {}", total_dist);    
     Ok(())
-}
+}    
