a77456bd53b4e576b8de4a45e154251021b22931
Phil Piwonka <wnka@users.noreply.github.com>
2023-12-07 19:05:59 -0800

    day 7 some progress

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{fs::File, io::{BufRead, BufReader}, str::Chars};
+use std::collections::HashSet;
 
 use clap::Parser;
 
@@ -36,6 +37,37 @@ fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead
     Ok(result)
 }
 
+#[derive(Debug)]
+struct Game {
+    input: String,
+    hand: Vec<u8>,
+    bid: u64,
+    hand_type: u8 // 5 of a kind = 5, 4 of a kind = 4, etc
+}
+
+impl Game {
+    fn new(input: &str, bid: u64) -> Self {
+        let mut hand = vec![];
+        for c in input.chars() {
+            let num_val = match c {
+                'A' => 14,
+                'K' => 13,
+                'Q' => 12,
+                'J' => 11,
+                'T' => 10,
+                _ => c.to_digit(10).unwrap()
+            };
+            hand.push(num_val as u8);
+        }
+
+        let set: HashSet<char> = HashSet::from_iter(input.chars().collect::<Vec<_>>());
+        let hand_type = set.iter().map(|v| input.matches(*v).count()).max().unwrap() as u8;
+
+        //let hand_type = HashSet::from_iter(test.chars().collect::<Vec<_>>());
+        Self { input: String::from(input), hand: hand, bid:bid, hand_type:hand_type }
+    }
+}
+
 fn main() -> Result<(), ParseError> {
     let args = Args::parse();
 
@@ -45,6 +77,11 @@ fn main() -> Result<(), ParseError> {
     for range in input_ranges {
         println!("{}", range);
     }
+
+    let test = "12345";
+
+    let hand = Game::new(test, 123);
+    println!("Hand: {:?}", hand);
 
     Ok(())
 }
