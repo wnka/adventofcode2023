27b3b3917625a303771ee018f0ef1a271fc2c850
Phil Piwonka <wnka@users.noreply.github.com>
2024-01-01 17:39:56 -0800

    More benchmark tests for day2

--- day2/benches/my_benchmark.rs
+++ day2/benches/my_benchmark.rs
@@ -1,15 +1,25 @@
 use std::{fs::File, io::BufReader};
+use std::io::prelude::*;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use nom::combinator::all_consuming;
+use nom::multi::many0;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let input_file = File::open("input.txt").unwrap();
     let input_ranges = day2::parse(BufReader::new(input_file)).unwrap();
 
-    c.bench_function("Parse input nom", |b| b.iter(|| {
+    let mut input_file = File::open("input.txt").unwrap();
+    let mut contents = String::new();
+    input_file.read_to_string(&mut contents).expect("Unable to read the file");
+
+    c.bench_function("Parse input nom many0", |b| b.iter(|| {
+        let _output = many0(day2::line_parser)(&contents);
+    }
+    ));
+    c.bench_function("Parse input nom loop", |b| b.iter(|| {
         for range in &input_ranges {
-            let _ = all_consuming(day2::line_parser)(&range);
+            let _ = all_consuming(day2::line_parser)(range);
         }
     }
     ));
